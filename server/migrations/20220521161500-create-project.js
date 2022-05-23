83814baaf42f7c032ecf0a1a9105c5d47dd51ea9
Ran Kim <rannio@gmail.com>
2022-05-22 21:31:19 -0400

    created model

---
+++ server/migrations/20220521161500-create-project.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      tech: {
+        type: Sequelize.STRING
+      },
+      github: {
+        type: Sequelize.STRING
+      },
+      site: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Projects');
+  }
+};
