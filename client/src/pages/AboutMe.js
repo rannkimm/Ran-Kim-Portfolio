f8fe8638940f205e4d10d2494f356f5756311276
Ran Kim <rannio@gmail.com>
2022-05-27 13:42:10 -0400

    styled aboutme

--- client/src/pages/AboutMe.js
+++ client/src/pages/AboutMe.js
@@ -1,9 +1,18 @@
 import AboutMeImg from '../images/aboutme.png'
+import Resume from '../images/resume.pdf'
+import TechImg from '../images/tech.png'
 
 const AboutMe = () => {
     return (
         <div className='aboutme-div'>
-            <img src={AboutMeImg} alt="aboutme" width={600}/>
+            <div className='resume-div'>
+                <img src={AboutMeImg} alt="aboutme" width={600}/>
+                <a href={Resume}>Resume</a>
+            </div>
+            <div className='techimg-div'>
+                <img src={TechImg} alt="aboutme" width={250}/>
+                
+            </div>
         </div>
     )
 }
